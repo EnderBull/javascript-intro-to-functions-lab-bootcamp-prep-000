c8dbafcfa8b54dde53b965f79e7e3d81f5863bd8
Simon A Bull <simonbull1@gmail.com>
2018-07-18 13:47:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,11 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string === lower)
+  if (string === string.toUpperCase()){
+    return ("YES INDEED!")
+  }
+  else if (string === string.toLowerCase()){
+    return ("I can't hear you!")
+  }
+  if (string === "I love you" )
 } 
