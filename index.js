58c3d748b2597c874f87efe654f09d8154958021
Simon A Bull <simonbull1@gmail.com>
2018-07-18 13:51:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,11 +23,7 @@ function sayHiToGrandma(string){
   else if (string === string.toLowerCase()){
     return ("I can't hear you!")
   }
-  if (string === "I love you" || "I love your, Grandma"){
+  if (string === "I love you, Grandma"){
     return ("I love you, too")
   }
 } 
-
-sayHiToGrandma("HELLO")
-sayHiToGrandma("hello")
-sayHiToGrandma("I love you")
